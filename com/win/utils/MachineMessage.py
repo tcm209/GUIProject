f186d99d1ce2c94c4d7b27c25d2f2fd96d6a535b
tcm209 <625201089@qq.com>
2019-01-29 14:58:03 +0800

    获取本机mac地址 作为权限验证

--- com/win/utils/MachineMessage.py
+++ com/win/utils/MachineMessage.py
@@ -2,11 +2,14 @@
 
 import uuid
 import socket
-
+# 获取电脑基本信息
 def _get_machine_message():
     # mac地址
     addr_num = hex(uuid.getnode())[2:]
     mac = "_".join(addr_num[i:i+2] for i in range(0,len(addr_num),2))
     print(mac)
     # 主机名称
-    host_name = socket.gethostbyname()
+    host_name = socket.gethostname()
+    machine_message=host_name+"_"+mac
+
+    return mac
