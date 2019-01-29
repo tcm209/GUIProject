1aae63a9d561716e3fadfad78e08564f4a11b118
tcm209 <625201089@qq.com>
2019-01-29 14:54:51 +0800

    读取yaml配置文件

--- com/win/utils/BaseConfigs.py
+++ com/win/utils/BaseConfigs.py
@@ -13,5 +13,19 @@ def _get_yaml():
     f.close()
     return s.decode() if isinstance(s, bytes) else s
 
+def _write_yaml(key,val):
+    path = os.path.join(os.path.dirname(__file__) + '/testconfig.yaml')
+    # 写入yaml 文件
+    # a 追加写入，w,覆盖写入
+    fw = open(path, 'a', encoding='utf-8')
+
+    # 构建数据
+    data = {
+        "REGIST": {'domain': '.yiyao.cc', 'expiry': 1521558688.480118, 'httpOnly': False, 'name': '_ui_', 'path': '/',
+                    'secure': False, 'value': 'HSX9fJjjCIImOJoPUkv/QA=='}}
+    # 装载数据
+    yaml.dump(data, fw)
+
+
 if __name__ == '__main__':
     print(_get_yaml())
