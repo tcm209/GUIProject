21fd7fcbe2da41ac24633b0ade7d9491b7a6845e
tcm209 <625201089@qq.com>
2019-01-31 10:11:31 +0800

    删除修改yaml配置文件

--- com/win/utils/BaseConfigs.py
+++ com/win/utils/BaseConfigs.py
@@ -1,30 +1,20 @@
 # -*- coding: utf8 -*-
 import os
 import yaml
+from ProjectURL import _get_project_dir
 
 def _get_yaml():
     """
     解析yaml
     :return: s  字典
     """
-    path = os.path.join(os.path.dirname(__file__) + '/baseconfig.yaml')
+    path = _get_project_dir() + "\\resource\\baseconfig.yaml"
     f = open(path, encoding='UTF-8')
     s = yaml.load(f)
     f.close()
     return s.decode() if isinstance(s, bytes) else s
 
-def _write_yaml(key,val):
-    path = os.path.join(os.path.dirname(__file__) + '/testconfig.yaml')
-    # 写入yaml 文件
-    # a 追加写入，w,覆盖写入
-    fw = open(path, 'a', encoding='utf-8')
 
-    # 构建数据
-    data = {
-        "REGIST": {'domain': '.yiyao.cc', 'expiry': 1521558688.480118, 'httpOnly': False, 'name': '_ui_', 'path': '/',
-                    'secure': False, 'value': 'HSX9fJjjCIImOJoPUkv/QA=='}}
-    # 装载数据
-    yaml.dump(data, fw)
 
 
 if __name__ == '__main__':
