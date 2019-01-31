8d32727aca10d22bcaf03b2a86b3cffd767d2ba4
tcm209 <625201089@qq.com>
2019-01-31 10:15:59 +0800

    把认证信息添加到xml文件

---
+++ com/win/utils/XmlUtils.py
@@ -0,0 +1,42 @@
+# -*- coding: utf8 -*-
+import xml.dom.minidom
+import os
+from ProjectURL import _get_project_dir
+
+
+#   获取element节点数据
+def _get_element_by_tag(tagname):
+    path = _get_project_dir() + "\\resource\\comfig.xml"
+    dom = xml.dom.minidom.parse(path)
+    root = dom.documentElement
+    elementval=root.getElementsByTagName(tagname)[0].firstChild.data
+
+    return elementval
+
+def _update_element_val(tagname,updateval):
+    path = _get_project_dir() + "\\resource\\comfig.xml"
+    dom = xml.dom.minidom.parse(path)
+    root = dom.documentElement
+    elementnode = root.getElementsByTagName(tagname)[0]
+    elementnode.firstChild.data=updateval
+    with open(path,'w') as fh:
+        dom.writexml(fh,encoding='utf-8')
+
+
+
+
+
+if __name__ == '__main__':
+    _get_element_by_tag("mac")
+
+    _update_element_val("phonenumber", 22)
+    _update_element_val("encryptstr", 22)
+
+    _update_element_val("mac", 33)
+    _update_element_val("registdate",44)
+    _update_element_val("timenumber", 55)
+    _update_element_val("key_encry", 66)
+    _update_element_val("isregisted", 0)
+
+
+
