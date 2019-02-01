4d715a553de6288aa2ffbad431a6c93546321d1b
tcm209 <625201089@qq.com>
2019-02-01 13:39:41 +0800

    修改项目根目录，解决pyinstaller打包成exe时路径报错

--- ProjectURL.py
+++ ProjectURL.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
-
+import sys
 def _get_project_dir():
-    project_dir = os.path.dirname(os.path.abspath(__file__))
+    dir = os.path.realpath(sys.argv[0])
+    project_dir = os.path.dirname(os.path.abspath(dir))
+    # project_dir = os.path.dirname(os.path.abspath(__file__))
     return project_dir
